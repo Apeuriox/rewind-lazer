82996ac6ce869bfeb444b3b2bd9bb89daf93f76f
Aloic <liuxusiu@foxmail.com>
2026-08-11 22:28:05 +0800

    added a button to create a temp wav audio file. This is meant to fix an audio latency bug. I searched for 3 hours found no way better than this forgive me pls....

--- libs/osu/pp/src/lib/diff.ts
+++ libs/osu/pp/src/lib/diff.ts
@@ -1,7 +1,6 @@
 // Strain
 import {
   Beatmap,
-  determineDefaultPlaybackSpeed,
   HitCircle,
   isHitCircle,
   isSlider,
@@ -295,10 +294,9 @@ const speedAdjustedOD = (OD: number, clockRate: number) =>
 
 // Calculates the different star ratings after every hit object i
 export function calculateDifficultyAttributes(
-  { appliedMods: mods, difficulty, hitObjects, controlPointInfo }: Beatmap,
+  { appliedMods: mods, difficulty, hitObjects, controlPointInfo, gameClockRate: clockRate }: Beatmap,
   onlyFinalValue: boolean,
 ) {
-  const clockRate = determineDefaultPlaybackSpeed(mods);
   const diffs = preprocessDifficultyHitObject(hitObjects, {
     clockRate,
     overallDifficulty: difficulty.overallDifficulty,
